70d38b7108245c94550479b033908cc7386d2d00
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-09-28 21:17:40 -0400

    move blender config to root

--- blender_hook/config.py
+++ blender_hook/config.py
@@ -7,7 +7,7 @@ CONFIG_FORMATTING = dict(
 )
 
 
-with open('blender_hook/config.json', 'r') as json_file:
+with open('blender_config.json', 'r') as json_file:
     config = json.load(json_file)
     for key, value in config.items():
         config[key] = value.format(**CONFIG_FORMATTING)
