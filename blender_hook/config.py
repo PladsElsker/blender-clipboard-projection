04c7ddb6c682f73cff5be6d4462106be5ef0d8ae
JohnWL <34081873+John-WL@users.noreply.github.com>
2024-09-29 22:52:23 -0400

    move hooks, ensure config doesn't throw if not present

--- blender_hook/config.py
+++ blender_hook/config.py
@@ -7,7 +7,10 @@ CONFIG_FORMATTING = dict(
 )
 
 
-with open('blender_config.json', 'r') as json_file:
-    config = json.load(json_file)
-    for key, value in config.items():
-        config[key] = value.format(**CONFIG_FORMATTING)
+try:
+    with open('blender_config.json', 'r') as json_file:
+        config = json.load(json_file)
+        for key, value in config.items():
+            config[key] = value.format(**CONFIG_FORMATTING)
+except:
+    config = None
